e2ed4267b9bfbddc3473cd9252bbb7a249d7bced
Tanjona <t.razanadrakoto@etechconsulting-mg.com>
2018-05-03 10:58:56 +0300

    component question

--- src/app/presentation/admin/admin-presentation.module.ts
+++ src/app/presentation/admin/admin-presentation.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '../../contrainte/config/shared/shared.module';
 import { ListQuizComponent } from './list-quiz/list-quiz.component';
 import { AdminRootComponent } from './admin-root/admin-root.component';
 import { CreateQuizComponent } from './create-quiz/create-quiz.component';
+import { QuizQuestionComponent } from './quiz-question/quiz-question.component';
 
 @NgModule({
   imports: [CommonModule, AdminRoutingModule, SharedModule],
-  declarations: [ListQuizComponent, AdminRootComponent, CreateQuizComponent]
+  declarations: [ListQuizComponent, AdminRootComponent, CreateQuizComponent, QuizQuestionComponent]
 })
 export class AdminPresentationModule {}
